656d60f39b8d304f365e29d3b85b8d0e45ea08b6
Kelvin Masambu <kevohmasambu@gmail.com>
2024-02-16 13:38:50 +0300

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h2>{movie.title}</h2>
+          <p>Time: {movie.time}</p>
+          <ul>
+            Genres:
+            {movie.genres.map((genre, i) => (
+              <li key={i}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
+
 
 export default Movies;
